e027ffb91156a318db706f8710801e812bcde263
Wes Reid <bwreid@gmail.com>
2016-04-18 13:04:54 -0600

    Allow for CORS

--- server/app.js
+++ server/app.js
@@ -24,6 +24,14 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 
+// *** allow for front-end requests *** //
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+
 // *** main routes *** //
 app.use('/', routes);
 app.use('/members', membersRoutes);
