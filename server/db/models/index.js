43dbb338faef70418592229c8798a67de4b6a0cd
Wes Reid <bwreid@gmail.com>
2016-04-17 21:00:40 -0600

    Add mongo db production url

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,5 +1,6 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/gdating-db');
+var db_url = process.env.MONGODB_URI || 'mongodb://localhost/gdating-db';
+mongoose.connect(db_url);
 
 module.exports.Member = require('./member');
 module.exports.Conversation = require('./conversation');
