1b2c40a836c4c413af7b1b9d815af9604537869a
Wes Reid <bwreid@gmail.com>
2016-04-18 23:50:03 -0600

    Add intermediary swagger docs for gdating api

--- server/gdating/routes/index.js
+++ server/gdating/routes/index.js
@@ -1,8 +1,13 @@
+var path = require('path');
 var express = require('express');
 var router = express.Router();
 
 router.get('/ping', function(req, res, next) {
   res.status(200).send({ message: 'OK' });
+});
+
+router.get('/api-docs', function(req, res) {
+  res.sendFile(path.join(__dirname + '/../swagger/index.html'));
 });
 
 module.exports = router;
