bd3a5efffdeea2f103b88cbf2f56cbaf239f4d12
Wes Reid <bwreid@gmail.com>
2016-04-21 12:44:31 -0600

    Update seed defaults for Conversations and Matches

--- server/gdating/db/seeds/conversations.js
+++ server/gdating/db/seeds/conversations.js
@@ -7,7 +7,7 @@ module.exports = seedData;
 
 function seedData (num) {
   var promises = [];
-  var toGenerate = num || 900;
+  var toGenerate = num || 5000;
 
   for ( var i = 0; i < toGenerate; i++ ) {
     var conversation = constructConversation()
