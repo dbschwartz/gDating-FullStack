bb45c5b2e9b98e8b7b20200eca1436fe56df715d
Michael Herman <hermanmu@gmail.com>
2016-04-23 16:00:23 -0600

    test updates

--- server/gdating/test/integration/test-conversations-routes.js
+++ server/gdating/test/integration/test-conversations-routes.js
@@ -45,4 +45,30 @@ describe('conversations routes', function() {
     });
   });
 
+  describe('POST /gdating/members/:id/conversations', function() {
+    it('should return a members\' conversations', function(done) {
+      Member.findOne()
+      .then(function(member) {
+        var memberID = member._id;
+        chai.request(server)
+        .post('/gdating/members/'+ memberID + '/conversations')
+        .send({
+            _recipient:1,
+            content:"1"
+          })
+        .end(function(err, res) {
+          res.status.should.equal(200);
+          res.type.should.equal('application/json');
+          res.body.should.be.a('object');
+          res.body.should.have.property('data');
+          res.body.data.should.be.a('Array');
+          res.body.data[0].should.have.property('_id');
+          res.body.data[0].should.have.property('messages');
+          res.body.data[0].should.have.property('_members');
+          done();
+        });
+      });
+    });
+  });
+
 });
